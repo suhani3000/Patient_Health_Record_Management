241bc07f2027258c96a2edc21560bff4adad465d
Suhani Satav <suhanisatav81@gmail.com>
2025-12-15 20:40:27 +0530

    Delete initial_blockchain directory

--- blockchain/ignition/modules/EHRRegistry.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("EHRRegistryModule", (m) => {
-  const ehrRegistry = m.contract("EHRRegistry");
-  return { ehrRegistry };
-});
